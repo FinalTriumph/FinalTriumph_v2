8d5c877083c4c8fecd6c22d5f657d37d887016d8
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-10-17 18:22:43 +0000

    commit

---
+++ public/js/About.js
@@ -0,0 +1,78 @@
+/* global $ */
+
+$(document).ready(function() {
+    
+    var expanded = 0;
+    var eventCount;
+    
+    $.getJSON(window.location.origin + "/api/events", function(data) {
+        
+        eventCount = data.length;
+        
+        for (var i = 0; i < data.length; i++) {
+            $("#events_desc").prepend('<div class="event_div"><p class="event_h_p" data-id="' + data[i]._id + '">' + data[i].time + ' <span class="event_h_sep">|</span> ' + data[i].title + '</p><p class="event_d_p ' + data[i]._id + '">' + data[i].description + '</p></div>');
+            $("#events_asc").append('<div class="event_div"><p class="event_h_p" data-id="' + data[i]._id + '">' + data[i].time + ' <span class="event_h_sep">|</span> ' + data[i].title + '</p><p class="event_d_p ' + data[i]._id + '">' + data[i].description + '</p></div>');
+        }
+        
+        $(".event_h_p").hover(function() {
+            $(this).parent().css("border-color", "#ddd");
+        }, function() {
+            $(this).parent().css("border-color", "#555");
+        });
+        
+        $(".event_h_p").click(function() {
+            var eId = $(this).attr('data-id');
+            if ($("."+eId).is(":visible")) {
+                $("."+eId).slideUp(300);
+                expanded -=1;
+                $(".exp_all").attr("disabled", false);
+                if (expanded === 0) {
+                    $(".cl_all").attr("disabled", "disabled");
+                }
+            } else {
+                $("."+eId).slideDown(300);
+                expanded +=1;
+                $(".cl_all").attr("disabled", false);
+                if (expanded === eventCount) {
+                    $(".exp_all").attr("disabled", "disabled");
+                }
+            }
+        });
+    });
+    
+    var visible = 'desc';
+    
+    $(".sw_tmln_dir").click(function() {
+        if (visible === 'desc') {
+            $("#events_desc").fadeOut(300, function() {
+                $("#events_asc").fadeIn(300);
+                visible = 'asc';
+            });
+        } else {
+            $("#events_asc").fadeOut(300, function() {
+                $("#events_desc").fadeIn(300);
+                visible = 'desc';
+            });
+        }
+    });
+    
+    $(".exp_all").click(function() {
+        $(".event_d_p").slideDown(300);
+        $(".exp_all").attr("disabled", "disabled");
+        $(".cl_all").attr("disabled", false);
+        expanded = eventCount;
+    });
+    
+    $(".cl_all").click(function() {
+        $(".event_d_p").slideUp(300);
+        $(".cl_all").attr("disabled", "disabled");
+        $(".exp_all").attr("disabled", false);
+        expanded = 0;
+    });
+    
+    $("#top_about_arrow").click(function() {
+        $("html, body").animate({
+            scrollTop: $("#timeline_content").offset().top
+        }, 1000);
+    });
+});
