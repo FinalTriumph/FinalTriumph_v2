cea57b28e89351dbc894825f78b745d1204256d2
Kaspars Andzans <kaspars_andzans@inbox.lv>
2017-10-25 17:45:14 +0000

    commit

--- public/js/Project.js
+++ public/js/Project.js
@@ -14,38 +14,50 @@ $(document).ready(function() {
             
             if (data[i]._id === id) {
                 
-                $("#single_project").append('<div id="sp_div_left"><h3 id="sp_h3">'+data[i].title+'</h3><p id="sp_p">'+data[i].description+'</p><div id="sp_lang">'+langDiv+'</div></div><div id="sp_div_right"><img src="'+data[i].image+'" alt="Project Image" id="sp_pr_img" /><a href="'+ data[i].link +'" id="sp_div_an" target="_blank">View Project</a></div>');
+                $("#single_project").append('<div id="sp_div_left"><h3 id="sp_h3">'+data[i].title+'</h3><p id="sp_p">'+data[i].description+'</p><div id="sp_lang">'+langDiv+'</div></div><div id="sp_div_right"><img src="'+data[i].image+'" alt="Project Image" id="sp_pr_img" /><br /><a href="'+ data[i].link +'" id="sp_div_an" target="_blank">View Project</a></div>');
                 document.title = data[i].title + ' | Kaspars Andžāns "FinalTriumph"';
                 
                 var catH3;
-                var catP;
                 
                 switch(data[i].category) {
                     case "new": 
-                        catH3 = "Other Newest Projects";
-                        catP = "Created with main focus on PHP / MySQL / Laravel.";
+                        catH3 = 'Other Newest Projects<br /><span class="pr_anounc_p">Created with main focus on <span class="gold_c">PHP</span> / <span class="gold_c">MySQL</span> / <span class="gold_c">Laravel</span>.</span>';
                         break;
                     case "fccfe": 
-                        catH3 = "Other Front End Projects for Free Code Camp";
-                        catP = "Created with main focus on HTML / CSS / JavaScript / jQuery.";
+                        catH3 = 'Other Front End Projects for Free Code Camp<br /><span class="pr_anounc_p">Created with main focus on <span class="gold_c">HTML</span> / <span class="gold_c">CSS</span> / <span class="gold_c">JavaScript</span> / <span class="gold_c">jQuery</span>.</span>';
                         break;
                     case "fccdv": 
-                        catH3 = "Other Data Visualization Projects for Free Code Camp";
-                        catP = "Created with main focus on D3.js / React.js.";
+                        catH3 = 'Other Data Visualization Projects for Free Code Camp<br/><span class="pr_anounc_p">Created with main focus on <span class="gold_c">D3.js</span> / <span class="gold_c">React.js</span>.</span>';
                         break;
                     case "fccbe": 
-                        catH3 = "Other Back End Projects for Free Code Camp";
-                        catP = "Created with main focus on Node.js / Express / MongoDB / Mongoose.";
+                        catH3 = 'Other Back End Projects for Free Code Camp<br /><span class="pr_anounc_p">Created with main focus on <span class="gold_c">Node.js</span> / <span class="gold_c">Express</span> / <span class="gold_c">MongoDB</span> / <span class="gold_c">Mongoose</span>.</span>';
                         break;
                 }
                 
                 $(".pr_anounc_h3").html(catH3);
-                $(".pr_anounc_p").html(catP);
             } else {
                 
-                $("#other_projects").prepend('<div class="project_div"><img src="' + data[i].image + '" alt="Project Image" class="project_left_side"/><div class="project_right_side"><h3 class="proj_div_h3">' + data[i].title + '</h3>' + langDiv + '<a href="' + window.location.origin + '/project?id=' + data[i]._id + '" class="pr_div_an">Project Description</a><a href="'+ data[i].link +'" class="pr_div_an" target="_blank">View Project</a></div></div>');
+                $("#other_projects").prepend('<div class="project_div"><div class="project_left_side"><h3 class="proj_div_h3">' + data[i].title + '</h3>' + langDiv + '<a href="' + window.location.origin + '/project?id=' + data[i]._id + '" class="pr_div_an">Project Description</a><a href="'+ data[i].link +'" class="pr_div_an" target="_blank">View Project</a></div><img src="' + data[i].image + '" alt="Project Image" class="project_right_side"/></div>');
             }
         }
+    });
+    
+    $(window).scroll(function(event) {
+        
+        var scrTop = $(this).scrollTop();
+        
+        if (scrTop > ($("#top_layer_2_bg").outerHeight() / 2 )) {
+            $("#btt_btn").show(500);
+        } else {
+            $("#btt_btn").hide(500);
+        }
+    
+    });
+    
+    $("#btt_btn").click(function() {
+        $("html, body").animate({
+            scrollTop: $("body").offset().top
+        }, 700);
     });
     
 })
